488e7f65c9fff06ebe5a194c5b7abedef3bcf653
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-04-09 23:23:30 +0200

    commit for news section creation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,6 +4,7 @@ use CodeIgniter\Router\RouteCollection;
 
 // Imported controllers
 use App\Controllers\Pages;
+use App\Controllers\News;
 
 /**
  * @var RouteCollection $routes
@@ -14,3 +15,7 @@ $routes->get('/', 'Home::index');
 // My Defined routes for news app:
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
+
+// News Routes
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/(:segment)', [News::class, 'show']);
